6146f73c69a922a47c6e56a3bcf231d225aacba8
Kai Hitawari <khitawari4@gmail.com>
2026-09-15 13:55:46 +0530

    fix: replace uuid package with native crypto randomUUID

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const db = require('../db');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { v4: uuidv4 } = require('uuid');
+const { randomUUID: uuidv4 } = require('crypto');
 const { JWT_SECRET } = require('../middleware/auth');
 const { sendOTPEmail } = require('../utils/mailer');
 
